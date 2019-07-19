e230e048a33d886590c19b4b806b61c9cf0edbc1
Hoàng Ngọc Hạnh <hngochanh@outlook.com>
2019-07-19 12:39:44 +0700

    update

--- certificate.sh
+++ certificate.sh
@@ -1,3 +1,15 @@
-openssl ecparam -genkey -name prime256v1 | openssl ec -out private.key
+
+wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
+sudo tar -xvf go1.12.6.linux-amd64.tar.gz
+sudo mv go /usr/local
+export GOROOT=/usr/local/go
+export PATH=$GOPATH/bin:$GOROOT/bin:$PATH
+
+go run generate_cert.go -ca --host "45.32.125.153"
+sudo rename cert.pem public.crt
+sudo rename key.pem private.key
+
+# openssl ecparam -genkey -name prime256v1 | openssl ec -out private.key
 sudo mkdir -p ${HOME}/.minio/certs
 cp -r -a private.key ${HOME}/.minio/certs
+cp -r -a public.crt ${HOME}/.minio/certs
