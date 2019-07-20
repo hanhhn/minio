e0c3cf44f503fc0e3061abf4bab67eb19b1e3d43
hanhhn <hngochanh@outlook.com>
2019-07-20 17:08:20 +0700

    update

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(eggs.vn www.eggs.vn image.eggs.vn file.eggs.vn)
+domains=(eggs.vn www.eggs.vn)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="" # Adding a valid address is strongly recommended
