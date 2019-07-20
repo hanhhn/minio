553e7b1e9dba3c4434d7b320e4cc329fe845ba56
hanhhn <hngochanh@outlook.com>
2019-07-20 15:04:15 +0700

    update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-sudo bash init-letsencrypt.sh
 sudo git pull
+sudo bash init-letsencrypt.sh
 sudo docker-compose -f docker-compose.yml up -d --build --remove-orphans --force-recreate
 sudo docker ps -a
